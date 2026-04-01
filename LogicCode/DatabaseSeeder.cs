f2d0ef916d7597d9d2d923764148fd4b669eea73
Ledana <ledanagjoka66@gmail.com>
2026-04-01 11:13:01 +0200

    created passwordhasher to hash and salt passwords and databaseseeder to put some test users into the database

---
+++ LogicCode/DatabaseSeeder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManagerWpf.LogicCode
+{
+    public class DatabaseSeeder
+    {
+        public static void SeedUsers(SqliteConnection connection)
+        {
+            string insertQuery = "INSERT INTO UserDetails (Name, UserName, UserId, PasswordHash, Balance) VALUES(@name, @userName, @userdId, @passwordHash, @balance)";
+
+            var testUsers = new List<(string Name, string UserName, string UserId, string Password, decimal Balance)>
+            {
+                ("Bob Dylan", "Dylan6790", "123456790", "Bob123", 0m),
+                ("Amelia Aster", "Aster6791", "123456791", "Amelia123", 150m),
+                ("Vivian Scott", "Scott6792", "123456792", "Vivian123", 80m),
+                ("Luiza Griffin", "Griffin6793", "123456793", "Luiza123", 0m),
+                ("Franceska DeMarti", "DeMarti6794", "123456794", "Francesca123", 0m),
+                ("Lory Marti", "Marti6795", "123456795", "Lory123", 700m),
+                ("Laila Martini", "Martini6796", "123456796", "Laila123", 250m),
+                ("Violet Vi", "Vi6797", "123456797", "Violet123", 0m)
+            };
+
+            foreach (var user in testUsers)
+            {
+                using (var cmd = new SqliteCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@name", user.Name);
+                    cmd.Parameters.AddWithValue("@userName", user.UserName);
+                    cmd.Parameters.AddWithValue("@userId", user.UserId);
+                    cmd.Parameters.AddWithValue("@passwordHash", PasswordHasher.HashPassword(user.Password));
+                    cmd.Parameters.AddWithValue("@balance", user.Balance);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ LogicCode/PasswordHasher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManagerWpf.LogicCode
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(16);
+            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100_000, HashAlgorithmName.SHA256);
+            byte[] hash = pbkdf2.GetBytes(32);
+
+            return Convert.ToBase64String(salt) + ":" + Convert.ToBase64String(hash);
+        }
+        public static bool VerifyPassword(string password, string storedHash)
+        {
+            var parts = storedHash.Split(':');
+            byte[] salt = Convert.FromBase64String(parts[0]);
+            byte[] storedSubkey = Convert.FromBase64String(parts[1]);
+
+            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100_000, HashAlgorithmName.SHA256);
+            byte[] computedSubkey = pbkdf2.GetBytes(32);
+
+            return storedSubkey.SequenceEqual(computedSubkey);
+        }
+    }
+}
