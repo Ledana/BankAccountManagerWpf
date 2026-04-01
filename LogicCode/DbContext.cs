ad8179b0cbce28f322b187306137563bba5a34cb
Ledana <ledanagjoka66@gmail.com>
2026-04-01 09:46:31 +0200

    Downloaded sqlite connection and created the dbcontext class

---
+++ LogicCode/DbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManagerWpf.LogicCode
+{
+    public class DbContext : IDisposable
+    {
+        private readonly SqliteConnection _connection;
+
+        public DbContext(string connectionString)
+        {
+            _connection = new(connectionString);
+            _connection.Open();
+        }
+
+        public SqliteConnection Connection => _connection;
+        public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+}
